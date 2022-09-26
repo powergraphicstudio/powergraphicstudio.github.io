0ab11aa95ebc3c4370ed0e3691bda022c9d97392
powergraphicstudio <www.pgstudio@gmail.com>
2022-09-26 17:36:02 +0330

    î▓b♀ⁿⁿⁿⁿ

--- noBootstrap/script.js
+++ noBootstrap/script.js
@@ -14,13 +14,50 @@ const swiper1 = new Swiper(".swiper1", {
 const swiper2 = new Swiper(".swiper2", {
     loop : true,
     spaceBetween: 30,
+    effect: "cube",
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+})
+const swiper3 = new Swiper(".swiper3", {
+    loop: true,
     effect: "fade",
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
     },
     autoplay: {
         delay: 3000,
         disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+})
+const swiper4 = new Swiper(".swiper4", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+})
+const swiper5 = new Swiper(".swiper5", {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: "fade",
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper4,
     },
 })
