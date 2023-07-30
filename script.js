d2e0a3870022a6f7433db6d2d1242e2f58b9dc94
powergraphicstudio <103212518+powergraphicstudio@users.noreply.github.com>
2023-07-30 14:45:11 +0330

    up

---
+++ script.js
@@ -0,0 +1,15 @@
+function insta() {
+    window.open("https://www.instagram.com/pg._studio/");
+  }
+
+  function tel() {
+    window.open("https://t.me/Pgstudio20");
+  }
+
+  function whats() {
+    window.open("https://wa.me/+989038797608");
+  }
+
+  function call() {
+    window.open("tel:989038797608");
+  }
